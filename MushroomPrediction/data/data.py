90233eac4e095594e2e5cdd22323c6e34ca03f5f
Kelvin Purdom <kelvin.purdom@gmail.com>
2022-12-30 13:52:33 +0100

    drop columns function

--- MushroomPrediction/data/data.py
+++ MushroomPrediction/data/data.py
@@ -9,17 +9,12 @@ def get_data():
 
 #removing empty data
 def clean_data(df, test=False):
-    df = df.dropna(how='any', axis='rows')
-    df = df[(df.dropoff_latitude != 0) | (df.dropoff_longitude != 0)]
-    df = df[(df.pickup_latitude != 0) | (df.pickup_longitude != 0)]
-    if "fare_amount" in list(df):
-        df = df[df.fare_amount.between(0, 4000)]
-    df = df[df.passenger_count < 8]
-    df = df[df.passenger_count >= 0]
-    df = df[df["pickup_latitude"].between(left=40, right=42)]
-    df = df[df["pickup_longitude"].between(left=-74.3, right=-72.9)]
-    df = df[df["dropoff_latitude"].between(left=40, right=42)]
-    df = df[df["dropoff_longitude"].between(left=-74, right=-72.9)]
+
+    drop_columns = ['gill-spacing', 'stem-root',
+                'stem-surface', 'veil-type',
+                'veil-color', 'spore-print-color',
+                ]
+    df.drop(columns=drop_columns, inplace=True)
     return df
 
 
