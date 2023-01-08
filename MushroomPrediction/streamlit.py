d965e3849d298655bfc3e20f9cc3bbef560f2f17
Kelvin Purdom <kelvin.purdom@gmail.com>
2023-01-08 13:30:50 +0100

    started streamlit page

--- MushroomPrediction/streamlit.py
+++ MushroomPrediction/streamlit.py
@@ -15,9 +15,7 @@ def predict(cap_diameter, cap_shape, cap_surface,
             stem_width, stem_color, has_ring,ring_type,
             habitat,season)
 
-
-
-prediction = model.predict(pd.DataFrame([[cap_diameter, cap_shape, cap_surface,
+    prediction = model.predict(pd.DataFrame([[cap_diameter, cap_shape, cap_surface,
             cap_color, does_bruise_or_bleed,
             gill_attachment,gill_color,stem_height,
             stem_width, stem_color, has_ring,ring_type,
@@ -27,4 +25,8 @@ prediction = model.predict(pd.DataFrame([[cap_diameter, cap_shape, cap_surface,
                                                  'gill-attachment', 'gill-color','stem-height',
                                                  'stem-width', 'stem-color', 'has-ring','ring-type',
                                                  'habitat','season']))
-return prediction
+    return prediction
+
+st.title('Poisonous Mushroom Predictor')
+st.image("""https://www.thestreet.com/.image/ar_4:3%2Cc_fill%2Ccs_srgb%2Cq_auto:good%2Cw_1200/MTY4NjUwNDYyNTYzNDExNTkx/why-dominion-diamonds-second-trip-to-the-block-may-be-different.png""")
+st.header('Enter the characteristics of the Mushroom:')
