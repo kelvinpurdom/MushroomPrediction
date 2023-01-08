627ff3ab38677d4ad733839087d71fadc3cfc937
Kelvin Purdom <kelvin.purdom@gmail.com>
2023-01-08 14:20:51 +0100

    streamlit is running!!!!

--- MushroomPrediction/app.py
+++ MushroomPrediction/app.py
@@ -1,19 +1,16 @@
-from sklearn.ensemble import RandomForestClassifier
 import streamlit as st
 import pandas as pd
 import pickle
-from sklearn.pipeline import Pipeline
 
-with open('../notebooks/rf_model.pkl', 'rb') as f:
+with open('rf_model.pkl', 'rb') as f:
     model = pickle.load(f)
 
-@st.cache
 
 def predict(cap_diameter, cap_shape, cap_surface,
             cap_color, does_bruise_or_bleed,
             gill_attachment,gill_color,stem_height,
             stem_width, stem_color, has_ring,ring_type,
-            habitat,season)
+            habitat,season):
 
     prediction = model.predict(pd.DataFrame([[cap_diameter, cap_shape, cap_surface,
             cap_color, does_bruise_or_bleed,
