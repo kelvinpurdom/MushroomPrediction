9a8c6aca4afca48be6b14098b52bfde5825746c6
Kelvin Purdom <kelvin.purdom@gmail.com>
2023-01-10 11:14:38 +0100

    updated setup instructions

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ requirements = [x.strip() for x in content if 'git+' not in x]
 
 setup(name='MushroomPrediction',
       version="1.0",
-      description="Project Description",
+      description="Poisonous Mushroom Predictor using a kaggle dataset with RandomForestClassifier",
       packages=find_packages(),
       install_requires=requirements,
       test_suite='tests',
