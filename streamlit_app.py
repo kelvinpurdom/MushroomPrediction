d80dfb89826fae0391981607fc422932a3336c4c
Kelvin Purdom <kelvin.purdom@gmail.com>
2023-01-26 20:03:22 +0100

    moved model to seperate file

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,40 +9,12 @@ from sklearn.preprocessing import OneHotEncoder
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
-
-preprocessor = ColumnTransformer([
-    ('num_encoder', MinMaxScaler(), make_column_selector(dtype_include="float64")),
-    ('cat_encoder', OneHotEncoder(handle_unknown='ignore', sparse=False), make_column_selector(dtype_include="object"))
-    ],remainder='passthrough')
-
-pipe = Pipeline([
-    ('preprocessing', preprocessor),
-    ('RandomForestClassifier', RandomForestClassifier(n_estimators=1000,
-                                                      criterion='gini',
-                                                      max_depth= 30,
-                                                      random_state=123)),
-])
-
-df = pd.read_csv('raw_data/secondary_data.csv', sep=";", low_memory=False)
-drop_columns = ['gill-spacing', 'stem-root',
-                'stem-surface', 'veil-type',
-                'veil-color', 'spore-print-color',
-                ]
-df.drop(columns=drop_columns, inplace=True)
-
-gender = {'p': 1,'e': 0}
-df['class'] = [gender[item] for item in df['class']]
-
-X = df.drop(columns='class')
-y = df['class']
-
-
-pipe.fit(X,y)
+from pipe import pipe_pre
 
 #model = Pipeline(pickle.load(open("rf_model.pkl","rb")))
 #pipe = joblib.load('rf_model.pkl')
 
-
+model = pipe_pre()
 # predict function will put all the variables from streamlit into the model
 def predict(cap_diameter, cap_shape, cap_surface,
             cap_color, does_bruise_or_bleed,
@@ -222,7 +194,7 @@ def predict(cap_diameter, cap_shape, cap_surface,
 
 
     # prediction of the model engaged
-    prediction = pipe.predict(pd.DataFrame([[cap_diameter, cap_shape, cap_surface,
+    prediction = model.predict(pd.DataFrame([[cap_diameter, cap_shape, cap_surface,
             cap_color, does_bruise_or_bleed,
             gill_attachment,gill_color,stem_height,
             stem_width, stem_color, has_ring, ring_type,
