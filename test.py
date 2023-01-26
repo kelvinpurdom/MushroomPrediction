a8728ed1b8e1f3bb847ba4b1a639e9ab5f3f803e
Kelvin Purdom <kelvin.purdom@gmail.com>
2023-01-26 15:34:20 +0100

    creating test.py

--- test.py
+++ test.py
@@ -1,9 +1,11 @@
 import streamlit as st
 import pandas as pd
 import pickle
-from sklearn.pipeline import Pipeline
+from transformers import pipeline
 
-model = Pipeline(pickle.load(open("rf_model.pkl","rb")))
+model = pickle.load(open("rf_model.pkl","rb"))
+pipe = pipeline(task="question-answering", model=model)
+
 
 # predict function will put all the variables from streamlit into the model
 def predict(cap_diameter, cap_shape, cap_surface,
@@ -184,7 +186,7 @@ def predict(cap_diameter, cap_shape, cap_surface,
 
 
     # prediction of the model engaged
-    prediction = model.predict(pd.DataFrame([[cap_diameter, cap_shape, cap_surface,
+    prediction = pipe.predict(pd.DataFrame([[cap_diameter, cap_shape, cap_surface,
             cap_color, does_bruise_or_bleed,
             gill_attachment,gill_color,stem_height,
             stem_width, stem_color, has_ring, ring_type,
